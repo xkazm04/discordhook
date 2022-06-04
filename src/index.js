414e421d4b55d89c845ef46a8395f8af41fa7552
xkazm04 <kazdan@seznam.cz>
2022-06-04 13:54:49 +0200

    Changed URL

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function composeSendMessageRequestAxiosConfig( data) {
     return {
         method: 'post',
-        url: 'https://discord.com/api/webhooks/980526309480480808/lqPjdvYJrnLxy0VuWxCC8_H5XDFbTR95ylkRiKoJ7DniPV9fdumAV_2b0qGjH2GeSvpD',
+        url: process.env.URL,
         headers: {
             'accept': 'application/json',
             'content-type': 'application/json'
