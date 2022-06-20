c2f16f73d9e84347703c30f0f416fe6de8a38e76
Oleg Antonyák <oleg.antonyak@tatum.io>
2022-06-20 19:34:50 +0200

    feat: loop

--- loop.js
+++ loop.js
@@ -6,56 +6,56 @@ const dotenv = require('dotenv');
 // If word found, Put API call to update contract
 dotenv.config();
 
-
 (async () => {
   try {
     // Fetch me CMS contracts - I will prolong query limit from 25 to 10000 after implementation
-    const getUrl = `${process.env.CMS_URL}`
+    const getUrl = `${process.env.CMS_URL}`;
     const data = await axios.get(getUrl);
     // Console return 3 results now
-    console.log(data.data)
-    // TBD loop through each contract
+    const contracts = data.data.data;
+    console.log('contracts', contracts);
 
-    
-    // For each contract start procedure
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // TBD replace by CMS data.data.attribute.reference
-    await page.goto('https://github.com/tintinweb/smart-contract-sanctuary-polygon/tree/master/contracts/mainnet/00/00000000011dF015e8aD00D7B2486a88C2Eb8210_DharmaKeyRingUpgradeBeaconControllerPolygon.sol');
-  
-    const res = await page.waitForFunction(
-      'document.querySelector("body").innerText.includes("OwnershipTransferred")'
-    );
-  
-    if(res!==null){
-      // ID to be replaced by CMS data.data.id
-      const id=3
-      console.log('Swap found')
-      const config = {
-        url: `${process.env.CMS_URL}${id}`,
-        headers: {
-            'accept': 'application/json',
+
+    // For each contract start procedure
+    for (const contract of contracts) {
+      const page = await browser.newPage();
+
+      page.setDefaultNavigationTimeout(0);
+      await page.goto(contract.attributes.reference);
+
+      // checks if text includes 'OwnershipTransferred'
+      const hasMatch = await page.evaluate(() =>
+        document
+          .querySelector('body')
+          .innerText.includes('OwnershipTransferred')
+      );
+
+      if (hasMatch) {
+        console.log('Swap found');
+        // ID to be replaced by CMS data.data.id
+        const id = 3;
+
+        const config = {
+          url: `${process.env.CMS_URL}${id}`,
+          headers: {
+            accept: 'application/json',
             'content-type': 'application/json',
             // 'Authorization': `Bearer ${process.env.API_TOKEN}` -- Endpoint public now without restriction
-        },
-        data:{
-          data:{isSwap: true}
+          },
+          data: {
+            data: { isSwap: true },
+          },
+        };
+
+        await axios.put(config.url, config.data, config.headers);
+        console.log('Swap updated');
+      } else {
+        console.log('Swap not found');
       }
-      }
-     
-      await axios.put(config.url,config.data,config.headers);
-      console.log('Swap updated')
-  
-    } else {
-      console.log('Swap not found')
     }
-    
-  
-  
     await browser.close();
   } catch (error) {
-    console.log(Object.keys(error), error.message); 
+    console.log(Object.keys(error), error.message);
   }
-
-
 })();
