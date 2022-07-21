9568d105429b41cf00a9615e3fadbd26faea1288
xkazm04 <kazdan@seznam.cz>
2022-07-21 13:25:08 +0200

    Row extraction functions

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
 const axios = require('axios');
 const dotenv = require('dotenv');
-const fs = require('fs');
 
 // 0. Extract row where text content = "pragma solidity" and add to array
 // 1. Extract row where text content = "function" or "event" and add to array
@@ -10,40 +9,71 @@ dotenv.config();
 
 (async () => {
   try {
-    // Fetch me CMS contracts - I will prolong query limit from 25 to 10000 after implementation
     // const getUrl = `${process.env.CMS_URL}`;
     // const data = await axios.get(getUrl);
     const data = await axios.get('https://d3v-center.herokuapp.com/api/contracts')
     const contracts = data.data.data;
-
     const browser = await puppeteer.launch();
 
     // For each contract start procedure
     for (const contract of contracts) {
       const page = await browser.newPage();
-
       page.setDefaultNavigationTimeout(0);
       await page.goto(contract.attributes.reference);
 
-      // marks contract per specific text 
+      // marks contract is valid and uses "pragma solidity"
       const findPragma = await page.evaluate(() =>
         document
           .querySelector('#repo-content-pjax-container > div > div > div.Box.mt-3.position-relative > div.Box-body.p-0.blob-wrapper.data.type-solidity.gist-border-0')
           .innerText.includes('pragma solidity')
       );
+      // extract whole body of Github code - done
+      const contractBody = await page.$eval(
+        "#repo-content-pjax-container > div > div > div.Box.mt-3.position-relative > div.Box-body.p-0.blob-wrapper.data.type-solidity.gist-border-0",
+        (element) => element.innerText
+      );
+      console.log('Extracted body ' + contractBody);
 
+
+
+
+      // TBD
       // https://www.digitalocean.com/community/tutorials/how-to-scrape-a-website-using-node-js-and-puppeteer
-      // Unsuccessful attempt to extract pragma
 
 
-       // Extracting file - example
-      let extractPragma = await page.$$eval('#repo-content-pjax-container > div > div > div.Box.mt-3.position-relative > div.Box-body.p-0.blob-wrapper.data.type-solidity.gist-border-0', rows => {
-        // Extract only row where word 'pragma' is present
-        rows = rows.filter(row => row.querySelector('table > tr').textContent == "*pragma*")
-        return links;
-      });
-      console.log(extractPragma);
+      // 1. Extract row where text contains some string
+      const version = await page.evaluate(() =>
+        Array.from(document.querySelectorAll("tr")).filter(rows => rows.innerText.indexOf('pragma') > -1).map(row => row.innerText)
+      )
+      console.log('Extracted version: '+ version)
 
+      // For each
+      // TBD clean row before posting - Trim, remove first characters
+      // TBD take only first value in array - 
+
+      // 1. Extract row where text contains some string
+      const author = await page.evaluate(() =>
+          Array.from(document.querySelectorAll("tr")).filter(rows => rows.innerText.indexOf('@author') > -1).map(row => row.innerText)
+        )
+          console.log('Extracted author: '+ author)
+      
+
+      const title = await page.evaluate(() =>
+          Array.from(document.querySelectorAll("tr")).filter(rows => rows.innerText.indexOf('@title') > -1).map(row => row.innerText)
+        )
+          console.log('Extracted title: '+ title)
+
+
+
+      // 2. Extract row where text contains some string and add to array
+      // Filter error, cannot use "includes" in this case 
+      // const events = await page.evaluate(() =>
+      //   Array.from(document.querySelectorAll("tr")).filter(rows => rows.innerText.indexOf('event') > -1).map(row => row.innerText)
+      // )
+      // console.log(events)
+    
+       // TBD clean row before posting - Trim, remove first characters
+       // TBD // Clean result - Each row will start with "event"/"function"
 
 
       // PUT Pragma version to CMS
