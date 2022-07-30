4712d5aacb201e06b7a88431e235f34f2b010cc4
xkazm04 <kazdan@seznam.cz>
2022-07-30 22:16:03 +0200

    fixed url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@
 
   (async () => {
     try {
-      const data = await axios.get('http://localhost:1337/api/contracts?pagination[limit]=50000')
+      const data = await axios.get(`${process.env.CMS_URL}?pagination[limit]=50000`)
       const contracts = data.data.data;
       console.log('Length: '+ contracts.length)
       
